02ebabeff0458f8b43a6dc4604107a1bc01af8e2
Vishnudxb <vishnudxb@protonmail.com>
2024-11-21 20:13:15 -0500

    cncf kochi

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # Make sure you install kind in your machine. https://kind.sigs.k8s.io/docs/user/quick-start#installation
 
-kind create cluster --name cluster1 --config kind-cluster1.yaml || echo "Cluster1 is running..."
-kind create cluster --name cluster2 --config kind-cluster2.yaml || echo "Cluster1 is running..."
+kind create cluster --name cluster1 --config ./kind-cluster1.yaml || echo "Cluster1 is running..."
+kind create cluster --name cluster2 --config ./kind-cluster2.yaml || echo "Cluster1 is running..."
 
 source ./context.sh
 
@@ -16,6 +16,11 @@ kubectl --context="$CTX_CLUSTER1" cluster-info dump | grep -E "cluster-cidr|serv
 # Cluster 2
 kubectl --context="$CTX_CLUSTER2" cluster-info dump | grep -E "cluster-cidr|service-cluster-ip-range"
 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml  --context="$CTX_CLUSTER1"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml  --context="$CTX_CLUSTER2"
+
+kubectl apply -f ./metallb-config1.yaml --context="$CTX_CLUSTER1" 
+kubectl apply -f ./metallb-config2.yaml --context="$CTX_CLUSTER2" 
 
 # Installing istio on both cluster
 
@@ -25,11 +30,3 @@ export PATH=$PWD/bin:$PATH
 
 istioctl version --context="$CTX_CLUSTER1"
 istioctl version --context="$CTX_CLUSTER2"
-
-
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb-native.yaml  --context="$CTX_CLUSTER1"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb-native.yaml  --context="$CTX_CLUSTER2"
-
-kubectl --context="$CTX_CLUSTER1" -f metallb-config1.yaml
-kubectl --context="$CTX_CLUSTER2" -f metallb-config2.yaml
-
